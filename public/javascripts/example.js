e988d6b51e5eb851a2a622f9de9c90aaf879be5a
Patricia Estridge <patricia.j.estridge@gmail.com>
2014-07-04 10:18:59 -0400

    initial commit for express node sit

---
+++ public/javascripts/example.js
@@ -0,0 +1,100 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var async = require('async');
+
+
+module.exports = { get_ratings: function(dict, callback){
+
+var url =  dict['url'];
+var our_text = [];
+var old_news_count = [];
+var num_reviews = 0;
+var price_range;
+var address_components;
+
+request(url, function(error, response, html){
+
+  if (!error && response.statusCode == 200){
+    var $ = cheerio.load(html);
+     $('span.price-range').each(function(i, element){ 
+        var p = $(this);
+        price_range = p.text();
+    });
+
+    $('p.review_comment').each(function(i, element){
+        var a = $(this);
+        our_text.push(a.text());
+    });
+
+
+    $('strong.street-address').each(function(i, element){
+
+        var ad = $(this).find('address');
+        address_components = [];
+        var i = 0;
+        ad.find('span').each(function(index){
+          if(i<2){
+        address_components.push(' '+$(this).text());
+        i++;
+      }
+      });
+
+    });
+  }
+  var all_text = our_text.join(" ");
+  old_news_count = all_text.match(/location|classic|line|institution|list|tourist/g);
+
+  
+  if (old_news_count != null){
+  old_count = old_news_count.length
+  }
+  else{
+  old_count = 0;
+  }
+
+  undiscovered_count = all_text.match(/little|authentic|hidden|hole in the wall|hole-in-the-wall|homemade|secret|gem|undiscovered|intimate|unique|different'/g);
+
+
+
+  if (undiscovered_count != null){
+  undiscovered_count = undiscovered_count.length
+  }
+  else{
+  undiscovered_count = 0;
+  }
+
+
+  introverted_count = all_text.match(/home|polite|welcoming|homemade|gourmet|date|ambience|intimate|quiet/g);
+
+
+ if (introverted_count != null){
+ introverted_count = introverted_count.length
+  }
+  else{
+  introverted_count = 0;
+  }
+
+
+  extroverted_count = all_text.match(/fucking|people watching|dj|DJ|upbeat|happy hour|drunk|party|loud|crowd/g);
+
+ 
+ if (extroverted_count != null){
+ extroverted_count = extroverted_count.length
+  }
+  else{
+  extroverted_count = 0;
+  }
+
+
+  dict['price_range'] = price_range;
+  dict['address'] = address_components;
+  dict['old_news'] = old_count;
+  dict['undiscovered'] = undiscovered_count;
+  dict['introverted'] = introverted_count;
+  dict['extroverted'] = extroverted_count;
+
+  callback(dict);
+});
+}
+
+}
---
+++ routes/index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+var apiJS = require('../public/javascripts/index');
+var home = require(__dirname, 'views/index');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Paddy and Chris' });
+});
+
+router.get('/accommodation', function(req, res) {
+  res.render('accommodation', { title: 'Paddy and Chris - Accommodation' });
+});
+
+router.get('/bridesmaids', function(req, res) {
+  res.render('bridesmaids', { title: 'Paddy and Chris - Bridesmaids' });
+});
+
+router.get('/gifts', function(req, res) {
+  res.render('gifts', { title: 'Paddy and Chris - Gifts' });
+});
+
+router.get('/travel', function(req, res) {
+  res.render('travel', { title: 'Paddy and Chris - Travel' });
+});
+
+router.get('/the_big_day', function(req, res) {
+  res.render('the_big_day', { title: 'Paddy and Chris - The Big Day' });
+});
+
+router.get('/photos', function(req, res) {
+  res.render('photos', { title: 'Paddy and Chris - Photos' });
+});
+
+router.get('/hen_party', function(req, res) {
+  res.render('hen_party', { title: 'Paddy and Chris - Hen Party' });
+});
+
+router.get('/ushers', function(req, res) {
+  res.render('ushers', { title: 'Paddy and Chris - Ushers' });
+});
+
+router.get('/stag_party', function(req, res) {
+  res.render('stag_party', { title: 'Paddy and Chris - Stag Party' });
+});
+
+router.get('/send', function(req, res) {
+  var category = req.query.category;
+  var location = req.query.location;
+  apiJS.get_info(category, location, function(data) {
+    console.log("routes data", data);
+    res.json(data);
+  });
+});
+
+module.exports = router;
